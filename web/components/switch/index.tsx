3cd0d18918b585f3d74c488da1382e4872bbeded
nmsn <136696700@qq.com>
2021-11-26 17:51:51 +0800

    修改样式

--- web/components/switch/index.tsx
+++ web/components/switch/index.tsx
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './index.module.less'
 
 interface SwitchProps {
   type?: 'horizontal' | 'vertical'
   len?: number
   num?: number
+  onChange: (key: number) => void
 }
 
 interface SwitchItemProps {
   isActive?: boolean
+  onChange?: (key: number) => void
+  value: number
 }
 
-const SwitchItem: React.FC<SwitchItemProps> = ({ isActive }) => {
-  return <div className={`${styles.switchItem} ${isActive && styles.active}`} />
+const SwitchItem: React.FC<SwitchItemProps> = ({
+  isActive,
+  onChange,
+  value,
+}) => {
+  return (
+    <div
+      className={`${styles.switchItem} ${isActive && styles.active}`}
+      onClick={() => onChange?.(value)}
+    />
+  )
 }
 
-const Switch: React.FC<SwitchProps> = () => {
+const Switch: React.FC<SwitchProps> = ({ num = 3, onChange }) => {
+  const [step, setStep] = useState(0)
+
+  const onCurChange = (key: number) => {
+    setStep(key)
+    onChange?.(key)
+  }
+
+  const data = new Array(num)?.fill('').map((item, index) => index)
+
   return (
     <div className={styles.switch}>
-      <SwitchItem isActive />
-      <SwitchItem />
-      <SwitchItem />
-      <SwitchItem />
+      {data.map((item) => (
+        <SwitchItem
+          isActive={step === item}
+          onChange={onCurChange}
+          value={item}
+        />
+      ))}
     </div>
   )
 }
--- web/pages/index/render.tsx
+++ web/pages/index/render.tsx
@@ -4,17 +4,34 @@ import { IData } from '~/typings/data'
 import { STORE_CONTEXT } from '_build/create-context'
 import Title from '@/components/title'
 import Card from '@/components/card'
+import Switch from '@/components/switch'
 import styles from './index.module.less'
+
+const ContentCard = () => {
+  return (
+    <div style={{ margin: 10 }}>
+      <Card title="测试数据">测试内容</Card>
+    </div>
+  )
+}
 
 export default function Index(props: SProps) {
   const { state, dispatch } = useContext<IContext<IData>>(STORE_CONTEXT)
+
+  const onChange = (key: number) => {
+    console.log(key)
+  }
+
   return (
     <div className={styles.container}>
       <Title>NMSN</Title>
       <div className={styles.content}>
-        <Card >测试内容</Card>
-        <Card title='测试数据'>测试内容</Card>
-        <Card title='测试数据'>测试内容</Card>
+        <div className={styles.left}>
+          <ContentCard />
+          <ContentCard />
+          <ContentCard />
+        </div>
+        <Switch onChange={onChange} num={5} />
       </div>
     </div>
   )
